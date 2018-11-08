22cfe20cd864b3fc7c0dfc42ea8d524ba4ddf65b
Omo <omosnewlife@gmail.com>
2018-11-08 13:24:38 +0100

    feat(api): Create POST endpoint
    - Create endpoint for POST '/api/v1/parcels'
    
    [finishes 161795045]

--- server/index.js
+++ server/index.js
@@ -9,7 +9,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/api/v1/parcels', (req, res) => {
+  if (req.body.pickupLocation && req.body.destination && req.body.description) {
+    const order = {
+      id: db.length + 1,
+      user: 'Omoefe',
+      status: 'created',
+      pickupLocation: req.body.pickupLocation,
+      currentLocation: req.body.pickupLocation,
+      destination: req.body.destination,
+      description: req.body.description,
+    };
+    db.push(order);
+    return res.status(201).send({
+      success: true,
+      message: 'Order created successfully.',
+      order,
+    });
+  }
 
+  return res.status(400).send({
+    success: false,
+    message: 'Please fill all fields.',
+  });
 });
 
 app.get('/api/v1/parcels', (req, res) => {
