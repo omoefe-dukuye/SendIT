b476ce8a88cdc3a2eda0601e763999f32a44a908
Omo <omosnewlife@gmail.com>
2018-11-13 21:03:00 +0100

    bg(ui): Implement LFA front-end feedback
    - Increase form input text
    - Remove on-focus form outline
    - Center navigation bar for pc view
    - Make form on login and admin page to match other forms
    - Create dummy parcel delivery orders
    - Reduce use of shadows
    - Improve contrast between header background and text
    
    [finishes 161886742]

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,31 +1,31 @@
 const nav = document.querySelectorAll('.nav');
 const doc = document.querySelectorAll('.doc');
-const profile = document.querySelector('.profile');
 const profileClose = document.querySelector('.profile_close');
 const seeProfile = document.querySelector('.see_profile');
 const blur = document.querySelector('.profile_wrapper');
 
+// go through each navigation tag and add listener for click event
 
-for (let i=0; i<nav.length; i++) {
+for (let i = 0; i < nav.length; i += 1) {
   nav[i].addEventListener('click', (e) => {
-    e.target.setAttribute('class', 'nav active');
-    for (let i=0; i<nav.length; i++) {
-      if (nav[i] !== e.target) {
-        nav[i].setAttribute('class', 'nav');
-        doc[i].setAttribute('class', 'doc no_display')
-      } else {
-        doc[i].setAttribute('class', 'doc');
+    e.target.setAttribute('class', 'nav active'); // on click add to 'active' class, show content
+    doc[i].setAttribute('class', 'doc');
+    for (let j = 0; j < nav.length; j += 1) {
+      // remove 'active' class from non-active tags, hide content
+      if (nav[j] !== e.target) {
+        nav[j].setAttribute('class', 'nav');
+        doc[j].setAttribute('class', 'doc no_display');
       }
     }
   });
 }
 
-seeProfile.addEventListener('click', () => {
+seeProfile.addEventListener('click', () => { // blur background on click
   blur.setAttribute('class', 'blur profile_wrapper');
-  profileClose.addEventListener('click', () => {
+  profileClose.addEventListener('click', () => { // cancel button reverses page to normal
     blur.setAttribute('class', 'profile_wrapper no_display');
   });
-  window.addEventListener('click', (e) => {
+  window.addEventListener('click', (e) => { // clicking anywhere outside pop-up reverses page to normal
     if (e.target === blur) {
       blur.setAttribute('class', 'profile_wrapper no_display');
     }
