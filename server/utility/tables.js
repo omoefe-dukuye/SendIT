417e140d41062f6885232536c3f7eb2dda696cf4
Omoefe Dukuye <omoefe.dukuye@gmail.com>
2018-11-19 17:46:38 +0100

    Test travis postgres service 7

--- server/utility/tables.js
+++ server/utility/tables.js
@@ -4,8 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const string = process.env.NODE_ENV === 'test'
+  ? 'postgres://czdovici:XcuWDGo0NA_moPDLotKnA6GD3ZkKD_FR@elmer.db.elephantsql.com:5432/czdovici'
+  : process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: 'postgres://czdovici:XcuWDGo0NA_moPDLotKnA6GD3ZkKD_FR@elmer.db.elephantsql.com:5432/czdovici',
+  connectionString: string,
 });
 
 pool.on('connect', () => {
