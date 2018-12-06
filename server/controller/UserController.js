9b5947e7e78f162aeecf4e4490037059c9ad3136
Omoefe Dukuye <omoefe.dukuye@gmail.com>
2018-12-06 18:04:19 +0100

    Refactor CreateUser module

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -16,16 +16,13 @@ class UserController {
   static async createUser(req, res) {
     try {
       const hashPassword = help.hashPassword(req.body.password);
-
       const {
         firstName, lastName, otherNames, email: mail, username: userName,
       } = req.body;
-
       const values = [
         firstName, lastName, otherNames, mail, userName, hashPassword,
         moment().format('MMMM Do YYYY, h:mm:ss a'),
       ];
-
       const {
         rows: [user], rows: [{
           id, first_name: userFirstName, last_name: userLastName,
@@ -39,17 +36,8 @@ class UserController {
       if (user.other_names === null) {
         delete user.other_names;
       }
-      return res.status(201).header('x-auth', token).json({
-        status: 201,
-        token,
-        user,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 500,
-        error,
-      });
-    }
+      return res.status(201).header('x-auth', token).json({ status: 201, token, user });
+    } catch (error) { res.status(500).json({ status: 500, error }); }
   }
 
   /**
@@ -61,18 +49,12 @@ class UserController {
     const { params: { userId: id }, body: { password } } = req;
     const { ADMIN_PASS: adminPass } = process.env;
     if (password !== adminPass) {
-      return res.status(401).json({
-        status: 401,
-        error: 'Invalid admin key',
-      });
+      return res.status(401).json({ status: 401, error: 'Invalid admin key' });
     }
     try {
       const { rows } = await db(selectById, [id]);
       if (!rows[0]) {
-        return res.status(400).json({
-          status: 400,
-          message: 'Invalid id',
-        });
+        return res.status(400).json({ status: 400, message: 'Invalid id' });
       }
       await db(upgradeToAdmin, ['yes', id]);
       return res.status(200).json({
@@ -80,10 +62,7 @@ class UserController {
         message: `User with the id '${id}' has been upgraded to admin`,
       });
     } catch (error) {
-      res.status(500).json({
-        status: 500,
-        error,
-      });
+      res.status(500).json({ status: 500, error });
     }
   }
 
@@ -95,38 +74,25 @@ class UserController {
   static async loginUser(req, res) {
     try {
       const { username, password: suppliedPassword } = req.body;
-
       const {
         rows: [user], rows: [{
           id, first_name: userFirstName, last_name: userLastName, email,
           password: existingPassword, is_admin: isAdmin,
         }]
       } = await db(selectByUsername, [username]);
-
       if (!user || !help.comparePassword(existingPassword, suppliedPassword)) {
-        return res.status(404).json({
-          error: 404,
-          message: 'invalid credentials',
-        });
+        return res.status(404).json({ error: 404, message: 'invalid credentials' });
       }
       const token = help.generateToken({
         id, userFirstName, userLastName, username, email, isAdmin,
       });
-
       delete (user.password);
       if (user.other_names === null) {
         delete user.other_names;
       }
-      return res.status(200).header('x-auth', token).json({
-        status: 200,
-        token,
-        user,
-      });
+      return res.status(200).header('x-auth', token).json({ status: 200, token, user });
     } catch (error) {
-      res.status(404).json({
-        status: 404,
-        message: 'invalid credentials',
-      });
+      res.status(404).json({ status: 404, message: 'invalid credentials' });
     }
   }
 }
